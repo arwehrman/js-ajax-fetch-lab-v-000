9f4a0920276c11a3040de5d18d917de6ff9fe3b7
Amanda Wehrman <arwehrman@gmail.com>
2018-02-09 01:00:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
-const token = 'YOUR_TOKEN_HERE';
-const postData = {
-  body: 'Great stuff'
-};
-
-fetch('https://api.github.com/repos/:your_ghname/:your_repo/commits/:sha/comments', {
-  method: 'POST',
-  body: JSON.stringify(postData),
-  headers: {
-    Authorization: `token ${token}`
-  }
-}).then(res => console.log(res));
-
-
 function getIssues() {
+  const repo = `https://api.github.com/repos/dschlauderaff/javascript-fetch-lab/issues`
+
+  fetch(repo, {
+    method: "get"
+  }).then(res => res.json()).then(json => showIssues(json))
 }
 
 function showIssues(json) {
+
+  let array = json.map(issue => `<li>${issue.title}</li><ul><li>${issue.body}</li></ul>`).join("")
+  $("#issues").append(`<ul>${array}</ul>`)
 }
 
 function createIssue() {
+  const repo = 'dschlauderaff/javascript-fetch-lab'
+  const token = getToken()
+  const issueTitle = document.getElementById('title').value
+  const issueBody = document.getElementById('body').value
+
+  const postData = {
+    title: `${issueTitle}`,
+    body: `${issueBody}`
+  }
+
+  fetch(`https://api.github.com/repos/${repo}/issues`, {
+    method: "post",
+    body: JSON.stringify(postData),
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => getIssues())
 }
 
 function showResults(json) {
@@ -26,7 +37,18 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const token = getToken()
   //use fetch to fork it!
+  fetch(`https://api.github.com/repos/${repo}/forks`, {
+    method: "post",
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => showForkedRepo(json))
+}
+
+function showForkedRepo(res){
+  $("#results").append(`<p><a href="${res.html_url}">${res.html_url}</a></p>`)
 }
 
 function getToken() {
