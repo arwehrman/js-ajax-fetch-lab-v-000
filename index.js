12dafec73ceed060549819361ed934733af668ef
Amanda Wehrman <arwehrman@gmail.com>
2018-02-09 01:57:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,17 @@ function showResults(json) {
 
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const url = 'https://api.github.com/repos/' + repo + '/forks'
   //use fetch to fork it!
+  fetch(url, {
+    method: 'post',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => {
+    console.log(res);
+    showForkedRepo(res.url)
+  })
 }
 
 function getToken() {
